095c6c6440085c1c03a2992c02a9c0d2cc9bd6da
Jared Novack <jarednova@upstatement.com>
2013-10-25 08:43:47 -0400

    rolled back to inital state

--- functions.php
+++ functions.php
@@ -8,14 +8,11 @@
 	add_filter('timber_context', 'add_to_context');
 
 	add_action('wp_enqueue_scripts', 'load_scripts');
-	add_action('wp_enqueue_scripts', 'load_styles');
 
 	register_nav_menu( 'primary', 'Primary Navigation' );
 
 	function add_to_context($data){
 		/* this is where you can add your own data to Timber's context object */
-		$data['menu'] = wp_nav_menu(array('menu' => 175, 'echo' => false));
-		$data['sidebar'] = Timber::get_sidebar();
 		return $data;
 	}
 
@@ -28,13 +25,5 @@
 	function load_scripts(){
 		wp_enqueue_script('jquery');
 	}
-
-	function load_styles(){
-		wp_register_style( 'screen', get_template_directory_uri().'/style.css', '', '', 'screen' );
-        wp_enqueue_style( 'screen' );
-	}
-
-
-
 
 
--- single.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$data = array();
-	$data['foo'] = 'bar';
-	$data['post'] = new TimberPost();
-	Timber::render('base.twig', $data);
